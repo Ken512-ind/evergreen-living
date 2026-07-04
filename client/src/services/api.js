a5550ab79b8d1a1ccfff2498c789ecd3c2d003c9
Ken512-ind <hafidznurhasan1@gmail.com>
2026-07-04 15:03:21 +0700

    Update favicon and deployment fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,23 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "http://202.155.13.202/api",
 });
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default api;
